869fab6c54702a88514ec848c73bd0496e66760c
Matthew Adam Knop <54086826+maknop@users.noreply.github.com>
2020-02-14 12:59:32 -0700

    Added Homepage w/ backbutton.

---
+++ TapTheDot/HomePage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace TapTheDot
+{
+    public partial class HomePage : ContentPage
+    {
+        public HomePage()
+        {
+            InitializeComponent();
+        }
+
+        async void Handle_Clicked(object sender, System.EventArgs e)
+        {
+            await Navigation.PopToRootAsync();
+        }
+    }
+}
