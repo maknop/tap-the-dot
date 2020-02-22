a52eef4f289a671a4e8bc14aa493ccbe204cb173
Matthew Adam Knop <54086826+maknop@users.noreply.github.com>
2020-02-22 12:14:49 -0700

    Added leadboard form & database.

---
+++ TapTheDot/Leaderboard.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+using SQLite;
+using Xamarin.Forms;
+
+namespace TapTheDot
+{
+    public class Leaderboard
+    {
+        public new int Id { get; set; }
+        public string Username { get; set; }
+        public int Score { get; set; }
+    }
+    public partial class Leaderboard : ContentPage
+    {
+        public Leaderboard()
+        {
+            InitializeComponent();
+
+            var connection = DependencyService.Get<ISQLiteDb>().GetConnection();
+        }
+    }
+}
