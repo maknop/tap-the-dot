975ad2a56529e583351e61b2888c00c9c18934e9
gmcfadden787 <60298305+gmcfadden787@users.noreply.github.com>
2020-02-25 17:16:44 -0700

    Updated settings page.

---
+++ TapTheDot/SettingsPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace TapTheDot
+{
+    public partial class SettingsPage : ContentPage
+    {
+        public SettingsPage()
+        {
+            InitializeComponent();
+        }
+
+        void NavigateButton_OnClicked1(object sender, EventArgs e)
+        {
+            App.Current.MainPage = new HomePage();
+        }
+    }
+}
