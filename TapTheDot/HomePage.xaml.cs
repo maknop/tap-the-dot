2d2026924fa78d42d7fc84d803cce0248b3a88d2
Matthew Adam Knop <54086826+maknop@users.noreply.github.com>
2020-02-28 12:01:24 -0700

    Navigates to 'New Game'.

--- TapTheDot/HomePage.xaml.cs
+++ TapTheDot/HomePage.xaml.cs
@@ -14,24 +14,24 @@ namespace TapTheDot
         }
 
 
+        // Navigates to the settings page
         void NavigateButton_OnClicked(object sender, EventArgs e)
         {
             App.Current.MainPage = new SettingsPage();
-
         }
 
+
+        // Navigates to the leaderboard page
         void Handle_Clicked1(object sender, EventArgs e)
         {
             App.Current.MainPage = new Leaderboard();
         }
 
-        public void ButtonSettings()
-        {
-            _ = new Button
-            {
-                Text = "Settings",
 
-            };
+        // Navigates to the 'New Game' screen
+        void NewGame_OnClicked(object sender, EventArgs e)
+        {
+            App.Current.MainPage = new GameScreen();
         }
     }
 }
