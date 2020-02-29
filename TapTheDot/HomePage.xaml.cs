7bb4d52b119ec47780c5b69a1a3ee3cd593ca1ed
Matthew Adam Knop <54086826+maknop@users.noreply.github.com>
2020-02-29 12:35:34 -0700

    Added navigation to game screen.

--- TapTheDot/HomePage.xaml.cs
+++ TapTheDot/HomePage.xaml.cs
@@ -14,15 +14,23 @@ namespace TapTheDot
         }
 
 
+        // Navigates to 'Settings' page
         void NavigateButton_OnClicked(object sender, EventArgs e)
         {
             App.Current.MainPage = new SettingsPage();
 
         }
 
+        // Navigates to 'Leaderboard' page
         void Handle_Clicked1(object sender, EventArgs e)
         {
             App.Current.MainPage = new Leaderboard();
+        }
+
+        // Navigates to 'GameScreen' Page
+        void GameScreen_onClicked(object sender, EventArgs e)
+        {
+            App.Current.MainPage = new GameScreen();
         }
 
         public void ButtonSettings()
