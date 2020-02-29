05688f117d1ad8d4fd5c196374eec3c3dfe76564
Matthew Adam Knop <54086826+maknop@users.noreply.github.com>
2020-02-29 11:12:08 -0700

    Created GameScreen form.

---
+++ TapTheDot/GameScreen.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace TapTheDot
+{
+    public partial class GameScreen : ContentPage
+    {
+        public GameScreen()
+        {
+            InitializeComponent();
+        }
+    }
+}
--- TapTheDot/HomePage.xaml.cs
+++ TapTheDot/HomePage.xaml.cs
@@ -14,15 +14,23 @@ namespace TapTheDot
         }
 
 
+        // Navigates to the 'Settings' screen
         void NavigateButton_OnClicked(object sender, EventArgs e)
         {
             App.Current.MainPage = new SettingsPage();
 
         }
 
+        // Navigates to the 'Leaderboard' screen
         void Handle_Clicked1(object sender, EventArgs e)
         {
             App.Current.MainPage = new Leaderboard();
+        }
+
+        // Navigates to the 'GameScreen' page
+        void GameScreen_onClicked(object sender, EventArgs e)
+        {
+            App.Current.MainPage = new GameScreen();
         }
 
         public void ButtonSettings()
