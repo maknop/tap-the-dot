1f1c484bfd8b3bbf88dfa036cd628b20da0c1921
KM <khadijomuhumed@hotmail.com>
2020-02-29 11:38:12 -0700

    Revert "v1"
    This reverts commit cfee530751c1f4e09ecc755b8e9eec51e523fffa, reversing
    changes made to c36c98c8295436332382841ffdf862f88bf924aa.

--- TapTheDot/HomePage.xaml.cs
+++ TapTheDot/HomePage.xaml.cs
@@ -52,28 +52,6 @@ namespace TapTheDot
         // Navigates to the 'GameScreen' page
         void GameScreen_onClicked(object sender, EventArgs e)
         {
-<<<<<<< HEAD
-            InitializeComponent();
-                
-        }
-
-
-        // Navigates to the 'Settings' screen
-        void NavigateButton_OnClicked(object sender, EventArgs e)
-        {
-            App.Current.MainPage = new SettingsPage();
-
-        }
-
-        // Navigates to the 'Leaderboard' screen
-        void Handle_Clicked1(object sender, EventArgs e)
-        {
-            App.Current.MainPage = new Leaderboard();
-        }
-
-        // Navigates to the 'GameScreen' page
-        void GameScreen_onClicked(object sender, EventArgs e)
-        {
             App.Current.MainPage = new GameScreen();
         }
 
@@ -92,26 +70,6 @@ namespace TapTheDot
         
        
  
-=======
-            App.Current.MainPage = new GameScreen();
-        }
-
-        public void ButtonSettings()
-        {
-            _ = new Button
-            {
-                Text = "Settings",
-
-            };
-        }
-    }
-}
-
-
-        
-       
- 
->>>>>>> ed1f3543fc8a95f6573c61854b3df29c81cbfc0f
 =======
             App.Current.MainPage = new GameScreen();
         }
