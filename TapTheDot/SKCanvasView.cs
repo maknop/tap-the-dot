beaa19a4487582844005c33f446848e174c92462
Matthew Adam Knop <54086826+maknop@users.noreply.github.com>
2020-03-05 19:47:38 -0700

    Create SKCanvasView.cs

---
+++ TapTheDot/SKCanvasView.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace TapTheDot
+{
+    internal class SKCanvasView
+    {
+        public int PaintSurface { get; internal set; }
+
+        public static implicit operator View(SKCanvasView v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
