5360e8e8d384d7f01373a35284327868a82c5e0d
Luke Monington <moni2990@bears.unco.edu>
2020-03-14 22:35:49 -0600

    Adding SkiaSharp to GameScreen
    Commented out the prior code which had the circle and other images in a grid. Then instead created an SKCanvas object and added some SkiaSharp code to draw a basic circle.

--- TapTheDot/GameScreen.xaml.cs
+++ TapTheDot/GameScreen.xaml.cs
@@ -25,7 +25,37 @@ namespace TapTheDot
 
         }
 
+        private void OnPainting(object sender, SKPaintSurfaceEventArgs e)
+        {
+            // we get the current surface from the event args
+            var surface = e.Surface;
+            // then we get the canvas that we can draw on
+            var canvas = surface.Canvas;
+            // clear the canvas / view
+            canvas.Clear(SKColors.White);
+            // create the paint for the filled circle
+            var circleFill = new SKPaint
+            {
+                IsAntialias = true,
+                Style = SKPaintStyle.Fill,
+                Color = SKColors.NavajoWhite
+            };
 
+            // draw the circle fill
+            canvas.DrawCircle(100, 100, 100, circleFill);
+
+            // create the paint for the circle border
+            var circleBorder = new SKPaint
+            {
+                IsAntialias = true,
+                Style = SKPaintStyle.Stroke,
+                Color = SKColors.Blue,
+                StrokeWidth = 5
+            };
+
+            // draw the circle border
+            canvas.DrawCircle(100, 100, 100, circleBorder);
+        }
         private void Button_ClickedBack(object sender, EventArgs e)
         {
             App.Current.MainPage = new HomePage();
@@ -47,9 +77,6 @@ namespace TapTheDot
             };
 
         }
-
-
-
 
     }
 
