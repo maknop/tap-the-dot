ab2694e0d05fc29df1f46fe243a54acc9b0f7815
Luke Monington <moni2990@bears.unco.edu>
2020-03-15 09:52:41 -0600

    Circle Improvements
    Added Scale and Transform to the circle to put it in the right place, and added a playerLine SKPaint.

--- TapTheDot/GameScreen.xaml.cs
+++ TapTheDot/GameScreen.xaml.cs
@@ -8,53 +8,75 @@ namespace TapTheDot
 {
     public partial class GameScreen : ContentPage
     {
-        SKBitmap helloBitmap;
+        //SKBitmap helloBitmap;
+        // create the paint for the filled circle
+        SKPaint circleFill = new SKPaint
+        {
+            IsAntialias = true,
+            Style = SKPaintStyle.Fill,
+            Color = SKColors.White
+        };
+        // create the paint for the circle border
+        SKPaint circleBorder = new SKPaint
+        {
+            IsAntialias = true,
+            Style = SKPaintStyle.Stroke,
+            Color = SKColors.Blue,
+            StrokeWidth = 5
+        };
 
+        SKPaint playerLine = new SKPaint
+        {
+            Style = SKPaintStyle.Stroke,
+            Color = SKColors.Red,
+            StrokeWidth = 8,
+            StrokeCap = SKStrokeCap.Round,
+            IsAntialias = true
+            // the strokeWidth is subject to the scale and transforms
+            // antialias makes it look nice
+        };
         public GameScreen()
         {
             InitializeComponent();
 
-            SKBitmap bitmap = new SKBitmap();
-            SKCanvas canvas = new SKCanvas(bitmap);
+            //SKCanvasView canvasView;
+            //SKBitmap circleBitmap;
+            //SKBitmap bitmap = new SKBitmap();
+            //SKCanvas canvas = new SKCanvas(bitmap);
 
 
-            using (canvas = new SKCanvas(bitmap))
-            {
-                // call drawing function
-            }
+            //using (canvas = new SKCanvas(bitmap))
+            //{
+            //    // call drawing function
+            //}
 
         }
 
         private void OnPainting(object sender, SKPaintSurfaceEventArgs e)
         {
             // we get the current surface from the event args
-            var surface = e.Surface;
+            SKSurface surface = e.Surface;
             // then we get the canvas that we can draw on
-            var canvas = surface.Canvas;
+            SKCanvas canvas = surface.Canvas;
             // clear the canvas / view
-            canvas.Clear(SKColors.White);
-            // create the paint for the filled circle
-            var circleFill = new SKPaint
-            {
-                IsAntialias = true,
-                Style = SKPaintStyle.Fill,
-                Color = SKColors.NavajoWhite
-            };
+            canvas.Clear(SKColors.CornflowerBlue);
 
-            // draw the circle fill
-            canvas.DrawCircle(100, 100, 100, circleFill);
+            int width = e.Info.Width;
+            int height = e.Info.Height;
 
-            // create the paint for the circle border
-            var circleBorder = new SKPaint
-            {
-                IsAntialias = true,
-                Style = SKPaintStyle.Stroke,
-                Color = SKColors.Blue,
-                StrokeWidth = 5
-            };
+            // Set transforms
+            canvas.Translate(width / 2, height / 2);
+            // dropping to width/20f makes it extremely large
+            canvas.Scale(width / 400f);
+
+            //// draw the circle fill
+            canvas.DrawCircle(0, 0, 100, circleFill);
 
             // draw the circle border
-            canvas.DrawCircle(100, 100, 100, circleBorder);
+            canvas.DrawCircle(0, 0, 150, circleBorder);
+
+            canvas.DrawLine(0, -100, 0, -150, playerLine);
+            // DrawLine from X1, Y1, to X2, Y2
         }
         private void Button_ClickedBack(object sender, EventArgs e)
         {
