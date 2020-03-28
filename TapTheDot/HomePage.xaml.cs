ca844b8beaa98927b8e5cf9f425974e07b55a3ff
Matthew Adam Knop <54086826+maknop@users.noreply.github.com>
2020-03-28 12:09:04 -0600

    Access to updating shared variables.

--- TapTheDot/HomePage.xaml.cs
+++ TapTheDot/HomePage.xaml.cs
@@ -7,6 +7,8 @@ namespace TapTheDot
 {
     public partial class HomePage : ContentPage
     {
+        SharedResources s = new SharedResources();
+
         public HomePage()
         {
             InitializeComponent();
@@ -20,18 +22,29 @@ namespace TapTheDot
 
         }
 
+
         // Navigates to 'Leaderboard' page
         void Handle_Clicked1(object sender, EventArgs e)
         {
             App.Current.MainPage = new Leaderboard();
         }
 
-        // Navigates to 'GameScreen' Page
+
+        // New Game Button, Navigates to 'GameScreen' Page
         void GameScreen_onClicked(object sender, EventArgs e)
         {
+            // Parameters: Score back to 0, Level back to 1
+            s.setScore(0);
+            s.setLevel(1);
             App.Current.MainPage = new GameScreen();
         }
 
+
+        // Resume Game, Navigates to 'GameScreen' Page
+        void Resume_onClicked(object sender, EventArgs e)
+        {
+            App.Current.MainPage = new GameScreen();
+        }
     }
 }
 
