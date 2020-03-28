2243e19c4ee7397895b47761286b51c5b79504e9
Matthew Adam Knop <54086826+maknop@users.noreply.github.com>
2020-03-28 12:09:30 -0600

    Ability to update shared resources.

---
+++ TapTheDot/SharedResources.cs
@@ -0,0 +1,62 @@
+﻿using System;
+namespace TapTheDot
+{
+    public class SharedResources
+    {
+        private int score;
+        private int level;
+
+        public SharedResources(int score, int level)
+        {
+            this.score = score;
+            this.level = level;
+        }
+
+
+        public SharedResources() { }
+
+
+        public int getScore()
+        {
+            return this.score;
+        }
+
+
+        public void setScore(int score)
+        {
+            this.score = score;
+        }
+
+
+        public int getLevel()
+        {
+            return this.level;
+        }
+
+
+        public void setLevel(int level)
+        {
+            this.level = level;
+        }
+
+
+        public void incrementScore()
+        {
+            this.score++;
+        }
+
+
+        public void incrementLevel()
+        {
+            this.level++;
+        }
+
+        /*
+        public void defaultValues()
+        {
+            this.score = 0;
+            this.level = 1;
+        }
+        */
+    }
+}
