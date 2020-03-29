045d6f85001fdd104d4d8473fa4a6a46c6d11c00
Luke Monington <moni2990@bears.unco.edu>
2020-03-29 12:34:47 -0600

    Reworked player movement
    Implemented System.Threading namespace in order to use the sleep method to pause the thread execution for a specified number of milliseconds.
    Added a "Speed" system which increases the player speed every 2 levels

--- TapTheDot/GameScreen.xaml.cs
+++ TapTheDot/GameScreen.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading;
 using SkiaSharp;
 using SkiaSharp.Views.Forms;
 using Xamarin.Forms;
@@ -10,27 +12,20 @@ namespace TapTheDot
     public partial class GameScreen : ContentPage
     {
         float randEnemy = 80;
-        float fastRotation = 0;
-        float rFastRotation = 0;
-        float mediumRotation = 0;
-        float rMediumRotation = 0;
-        float slowRotation = 0;
-        float rSlowRotation = 0;
-        float slowestRotation = 0;
-        float rSlowestRotation = 0;
-        float currentRotation = 0;
-        float x = 0;
+        float currentRotation = 720;
         bool reverse = false;
         int score = 0;
         int level = 1;
-        float positionCorrection = 0;
-        //// create the paint for the filled circle
+        int speed = 1;
+
+        // create the paint for the filled circle
         SKPaint circleFill = new SKPaint
         {
             IsAntialias = true,
             Style = SKPaintStyle.Fill,
             Color = SKColors.White
         };
+
         // create the paint for the circle border
         SKPaint circleBorder = new SKPaint
         {
@@ -64,13 +59,13 @@ namespace TapTheDot
             InitializeComponent();
             // In order for the player to continually move, we need to ensure the paint surface event handler is repeatedly executed
             // We want the timer to refresh 60 times per second, since that is the typical refresh rate of most monitors
-
             Device.StartTimer(TimeSpan.FromSeconds(1f / 144), () =>
               {
                   canvasView.InvalidateSurface();
                   return true;
               });
         }
+
         private float randValue()
         {
             Random cRand = new Random();
@@ -96,54 +91,30 @@ namespace TapTheDot
             // draw the circle fill and border
             canvas.DrawCircle(0, 0, 100, circleFill);
             canvas.DrawCircle(0, 0, 150, circleBorder);
-
-
-            float position = randValue();
-            // Instantiate Date/Time
-            DateTime dateTime = DateTime.Now;
-            float milliseconds = dateTime.Millisecond;
-            float seconds = dateTime.Second;
-
-            // formula: (seconds % [# rotations per second] * [360 / # rotations per second]) + (milliseconds / (float)[1000 / (360 / # rotations per second)]
-            // rotates once per second
-            //float Rotation = milliseconds / (float)2.77777778;
-            // rotates once every 2 seconds
-            this.fastRotation = (seconds % 2 * 180) + (milliseconds / (float)5.5555556);
-            //this.rFastRotation = -((seconds % 2 * 180) + (milliseconds / (float)5.5555556));
-            // rotates once every 3 seconds
-            this.mediumRotation = (seconds % 3 * 120) + (milliseconds / (float)8.333333333);
-            //this.rMediumRotation = -((seconds % 3 * 120) + (milliseconds / (float)8.333333333));
-            // rotates once every 4 seconds
-            this.slowRotation = (seconds % 4 * 90) + (milliseconds / (float)11.11111111);
-            //this.rSlowRotation = -((seconds % 4 * 90) + (milliseconds / (float)11.11111111));
-            // rotates once every 5 seconds
-            this.slowestRotation = (seconds % 5 * 72) + (milliseconds / (float)13.88888889);
-            currentRotation = slowestRotation;
-            //x = -((seconds % 5 * 72) + (milliseconds / (float)13.88888889)) + 360;
-            //if (slowestRotation <= 180)
-            //{
-            //    //rSlowestRotation = (-((seconds % 5 * 72) + (milliseconds / (float)13.88888889))+360)-((-((seconds % 5 * 72) + (milliseconds / (float)13.88888889)) + 360)-slowestRotation);
-            //    rSlowestRotation = x - (x + slowestRotation);
-            //}
-            //if (slowestRotation > 180)
-            //{
-            //    rSlowestRotation = x + (slowestRotation - x);
-            //}
-
-            DebugLabel.Text = "Enemy loc: " + randEnemy.ToString() + "Player loc: " + currentRotation;
-            //if (level == 1 && reverse == false)
-            //{
-            //    currentRotation = slowestRotation;
-            //}
-            //if (level == 1 && reverse == true)
-            //{
-            //    currentRotation = rSlowestRotation;
-            //}
-
-
-
+            // Since we are making this game for 60 frames per second, we want it to update 60 times per second
+            // the sleep function tells the computer how many milliseconds to wait before updating again. We want this to be 1000 milliseconds / 60 fps ~ 17 milliseconds
+            Thread.Sleep(17);
+            MainLabel.Text = "Score: " + score.ToString();
+            LevelLabel.Text = "Level: " + level.ToString();
+            DebugLabel.Text = "Speed: " + speed.ToString();
+            // Make sure the current rotation never becomes negative, because a negative value would mess up the "hit detection"
+            if (currentRotation <= 0)
+            {
+                currentRotation += 360;
+            }
             // We want to call the canvas.Save() method before the rotating the player line and then the canvas.Restore() method after
             canvas.Save();
+            if (reverse == false)
+            {
+                currentRotation += speed * 2;
+            }
+            if (reverse == true)
+            {
+                currentRotation -= speed * 2;
+            }
+            
+
+
             // Canvas.Rotate Degrees will rotate the canvas by the specified number of degrees. We want this number to count up to exactly 360 for a full circle
             canvas.RotateDegrees(currentRotation);
             // DrawLine will draw a line from from X1, Y1, to X2, Y2
@@ -175,7 +146,6 @@ namespace TapTheDot
                 Color = Color.Red.ToSKColor(),
                 StrokeWidth = 25
             };
-
         }
         private float randMovement()
         {
@@ -188,24 +158,24 @@ namespace TapTheDot
         {
             if ((currentRotation%360 )+ 27 > randEnemy && (currentRotation%360) - 27 < randEnemy)
             {
-
                 randEnemy = randMovement() * 360;
                 score += 1;
-                MainLabel.Text = "Score: " + score.ToString();
-                LevelLabel.Text = "Level: " + level.ToString();
-                if (score % 5 == 4)
+                if (score % 5 == 0)
                 {
                     level += 1;
                 }
-                //if (score % 2 == 1)
-                //{
-                //    reverse = true;
-                //}
-                //if (score % 2 == 0)
-                //{
-                //    reverse = false;
-                //}
-
+                if (score % 10 == 0)
+                {
+                    speed += 1;
+                }
+                if (score % 2 == 1)
+                {
+                    reverse = true;
+                }
+                if (score % 2 == 0)
+                {
+                    reverse = false;
+                }
             }
 
         }
