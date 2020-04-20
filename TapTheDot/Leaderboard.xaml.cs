c83a095f697eb5ac61c1a7d032efa974341a49a6
Matthew Adam Knop <54086826+maknop@users.noreply.github.com>
2020-04-20 01:04:52 -0600

    Testing listview, button functionality, some Documentation.

--- TapTheDot/Leaderboard.xaml.cs
+++ TapTheDot/Leaderboard.xaml.cs
@@ -6,24 +6,74 @@ using Xamarin.Forms;
 
 namespace TapTheDot
 {
-    public partial class Leaderboard : ContentPage
+    /*
+     * UserScores class
+     *
+     * Purpose: Gettors and Settors for items added to the database.
+     */
+    public class UserScores
     {
-        public Leaderboard(int id, string username, int score)
-        {
-            this.Id = id;
-            this.Username = username;
-            this.Score = score;
-        }
-
         public new int Id { get; set; }
         public string Username { get; set; }
         public int Score { get; set; }
+    }
 
+    /*
+     * Leaderboard class
+     *
+     * Purpose: Displays the leaderboard and adds new scores and usernames to
+     *          the database to be displayed.
+     */
+    public partial class Leaderboard : ContentPage
+    {
+        // Constructor
         public Leaderboard()
         {
             InitializeComponent();
 
-            //var connection = DependencyService.Get<ISQLiteDb>().GetConnection();
+            var connection = DependencyService.Get<ISQLiteDb>().GetConnection();
+
+            var names = new List<string> {
+                "Matt",
+                "Luke",
+                "Gwen",
+                "Khadijo"
+            };
+            listView.ItemsSource = names;
         }
+
+
+        void ToMainPage(object sender, EventArgs e)
+        {
+            App.Current.MainPage = new HomePage();
+        }
+
+
+        void ToNewGame(object sender, EventArgs e)
+        {
+            App.Current.MainPage = new GameScreen();
+        }
+
+
+        // Adds to the database
+        void add(object sender, System.EventArgs e)
+        {
+
+        }
+
+
+        // Updates a line in the database
+        void update(object sender, System.EventArgs e)
+        {
+
+        }
+
+
+        // Deletes a line from the database
+        void delete(object sender, System.EventArgs e)
+        {
+
+        }
+
     }
 }
