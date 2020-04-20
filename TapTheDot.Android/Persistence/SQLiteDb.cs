c71be799d8464aba3e10e66db59143895156054e
Matthew Adam Knop <54086826+maknop@users.noreply.github.com>
2020-04-19 22:59:58 -0600

    Created implementation of the interface for iOS & Android.

---
+++ TapTheDot.Android/Persistence/SQLiteDb.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+using SQLite;
+using Xamarin.Forms;
+using TapTheDot.Android;
+
+[assembly: Dependency(typeof(SQLiteDb))]
+
+namespace TapTheDot.Android
+{
+    public class SQLiteDb : ISQLiteDb
+    {
+        public SQLiteAsyncConnection GetConnection()
+        {
+            var documentsPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            var path = Path.Combine(documentsPath, "MySQLite.db3");
+
+            return new SQLiteAsyncConnection(path);
+        }
+    }
+}
---
+++ TapTheDot.iOS/Persistence/SQLiteDb.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+using SQLite;
+using Xamarin.Forms;
+using TapTheDot.iOS;
+
+[assembly: Dependency(typeof(SQLiteDb))]
+
+namespace TapTheDot.iOS
+{
+    public class SQLiteDb : ISQLiteDb
+    {
+        public SQLiteAsyncConnection GetConnection()
+        {
+            var documentsPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            var path = Path.Combine(documentsPath, "MySQLite.db3");
+
+            return new SQLiteAsyncConnection(path);
+        }
+    }
+}
