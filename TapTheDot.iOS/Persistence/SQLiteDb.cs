0e136e5ac630f377123e7ad95021343617818362
Matthew Adam Knop <54086826+maknop@users.noreply.github.com>
2020-04-24 23:13:21 -0600

    Revert "Revert "Fixed button sizing.""
    This reverts commit 97359c9808ee6c76423d4d97cf8497e1e0515540.

--- TapTheDot.iOS/Persistence/SQLiteDb.cs
+++ TapTheDot.iOS/Persistence/SQLiteDb.cs
@@ -12,7 +12,7 @@ namespace TapTheDot.iOS
     {
         public SQLiteAsyncConnection GetConnection()
         {
-            var documentsPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            var documentsPath = Environment.GetFolderPath(System.Environment.SpecialFolder.Personal);
             var path = Path.Combine(documentsPath, "MySQLite.db3");
 
             return new SQLiteAsyncConnection(path);
