b2ba8b124ca0c154d04645de15f320afb5ecf7b6
Matthew Adam Knop <54086826+maknop@users.noreply.github.com>
2020-04-30 23:54:42 -0600

    Added labels, text entry, and buttons. Still issues with displaying data.

--- TapTheDot/Leaderboard.xaml.cs
+++ TapTheDot/Leaderboard.xaml.cs
@@ -1,10 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using SQLite;
 using Xamarin.Forms;
-using System.Linq;
-using System.ComponentModel;
 
 namespace TapTheDot
 {
@@ -13,23 +10,22 @@ namespace TapTheDot
      *
      * Purpose: Gettors and Settors for items added to the database.
      */
-    public class Users : INotifyPropertyChanged
-    {
-        public event PropertyChangedEventHandler PropertyChanged;
-
+    public class Users
+    { 
         [PrimaryKey, AutoIncrement, Column("ID")]
         public int Id { get; set; }
-        
+
         [Column("Username")]
         public String Username { get; set; }
 
-        
         [Column("Score")]
         public int Score { get; set; }
 
         [Column("Level")]
         public int LevelAchieved { get; set; }
+        
     }
+
 
     /*
      * Leaderboard class
@@ -48,56 +44,60 @@ namespace TapTheDot
         public Leaderboard()
         {
             InitializeComponent();
-            //UserInput.Text = "Username: " + EndScreen.userInput;
 
             _connection = DependencyService.Get<ISQLiteDb>().GetConnection();
         }
 
 
         protected override async void OnAppearing()
-        //protected new async void OnAppearing()
         {
-            //_connection.DropTableAsync<Players>();
+            //username.Text = inputName.ToString();
+            //score.Text = GameScreen.score.ToString();
+            //Level.Text = GameScreen.level.ToString();
+
+
+            //_connection.DropTableAsync<Users>();
             await _connection.CreateTableAsync<Users>();
 
+            var usersList = await _connection.Table<Users>().ToListAsync();
+            allUsers = new ObservableCollection<Users>(usersList);
 
-            // This was printing something to listview.
-            /*
-            userListView.ItemsSource = new List<Players>
-            {
-                new Players {
-                    Username = EndScreen.userInput,
-                    Score = GameScreen.score,
-                    LevelAchieved = GameScreen.level
-                }
-            };
-            */
-
-
-
-            var users = await _connection.Table<Users>().ToListAsync();
-            allUsers = new ObservableCollection<Users>(users);
             userListView.ItemsSource = allUsers;
-
             base.OnAppearing();
+           
         }
 
 
         // Adds to the database
-        async void AddPlayer(object sender, EventArgs e)
+        async void AddPlayer(object sender, System.EventArgs e)
         {
-            var userList = new Users
+            if (GameScreen.score > 0)
             {
-                Username = EndScreen.userInput,
-                Score = GameScreen.score,
-                LevelAchieved = GameScreen.level
-            };
+                var userList = new Users
+                {
+                    Username = inputName.ToString(),
+                    Score = GameScreen.score,
+                    LevelAchieved = GameScreen.level
+                };
 
-            await _connection.InsertAsync(userList);
+              
 
-            allUsers.Add(userList);
+                await _connection.InsertAsync(userList);
+                //await _connection.UpdateAsync(userList);
+
+                allUsers.Add(userList);
+
+            
+                GameScreen.reverse = false;
+                GameScreen.score = 0;
+                GameScreen.tracker = 0;
+                GameScreen.level = 1;
+                GameScreen.speed = 1;
+                GameScreen.lives = 10;
+                App.Current.MainPage = new HomePage();
+            }
         }
-
+        
 
         // 'Main Page' button functionality.
         void ToMainPage(object sender, EventArgs e)
@@ -117,6 +117,17 @@ namespace TapTheDot
             GameScreen.lives = 10;
             App.Current.MainPage = new GameScreen();
         }
-        
+
+
+        void OnEntryTextChanged(object sender, TextChangedEventArgs e)
+        {
+            string oldText = e.OldTextValue;
+            string newText = e.NewTextValue;
+        }
+
+        void OnEntryCompleted(object sender, EventArgs e)
+        {
+            String userInput = ((Entry)sender).Text;
+        }
     }
 }
